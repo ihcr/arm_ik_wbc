using quaternions for Cartesian task and fixed gain error.

python/arm_ik_wbc/ik_wbc.py
@@ -315,9 +315,9 @@ class IkWBC:
                 #constraints = constraints + current_constraints
                 indx = targets_dict["task name"].index(self.active_constraint_dict["constraint name"][i])
                 for ii in range(len(targets_dict["target pos"][indx])):
-                    if targets_dict["target pos"][indx][ii] == self.prev_target["target pos"][indx][ii]:
-                        constraints_pos[ii] = True
-                    if targets_dict["target ori"][indx][ii] == self.prev_target["target ori"][indx][ii]:
+                    #if targets_dict["target pos"][indx][ii] == self.prev_target["target pos"][indx][ii]:
+                        #constraints_pos[ii] = True
+                    if targets_dict["target ori"][indx][ii] == self.prev_target["target ori"][indx][ii] and ii == 0:
                         constraints_ori[ii] = True      
                 constraints = constraints_pos + constraints_ori
                 self.active_constraint_dict["constraint instance"][i].updateConstraintParameters(self.active_constraint_dict["constraint instance"][i].frame_index, self.active_constraint_dict["constraint instance"][i].reference_frame, constraints)
@@ -385,9 +385,9 @@ class IkWBC:
         task_parameters = []
         
         gain = np.identity(6)
-        gain[3,3] = 1
-        gain[4,4] = 1
-        gain[5,5] = 1
+        gain[3,3] = 0.00
+        gain[4,4] = 0.00
+        gain[5,5] = 0.00
         
         for i in range(self.n_of_EE):
             task_names.append(self.EE_joint_names[i])
@@ -397,14 +397,14 @@ class IkWBC:
         for i in range(self.n_velocity_dimensions):
             task_names.append("joint " + str(i))
             task_types.append("Joint")
-            task_parameters.append([i, self.n_velocity_dimensions, 0.00001, "PREV", 0, self.init_joint_config])
+            task_parameters.append([i, self.n_velocity_dimensions, 0.001, "PREV", 0, self.init_joint_config])
         
         # for high dof arms, soft pose control may be required  
         if high_dof == True:
             for i in range(self.n_velocity_dimensions-self.n_of_manip_joints):
                 task_names.append("joint pose" + str(i))
                 task_types.append("Joint")
-                task_parameters.append([i, self.n_velocity_dimensions, 0.1, "POSE", 0, self.init_joint_config])
+                task_parameters.append([i, self.n_velocity_dimensions, 0.5, "POSE", 0, self.init_joint_config])
             
         for i in range(len(task_names)):
             task_dict["task name"].append(task_names[i])
@@ -422,7 +422,7 @@ class IkWBC:
         for i in range(self.n_of_EE):
             cstrnt_names.append(self.EE_joint_names[i])
             cstrnt_types.append("Cart")
-            cstrnt_parameters.append([self.EE_index_list_frame[i], pin.ReferenceFrame.LOCAL_WORLD_ALIGNED, False, False, False, False, False, False, True])
+            cstrnt_parameters.append([self.EE_index_list_frame[i], pin.ReferenceFrame.LOCAL_WORLD_ALIGNED, False, False, False, False, False, False, False])
         for i in range(len(cstrnt_names)):
             cstrnt_dict["constraint name"].append(cstrnt_names[i])
             cstrnt_dict["constraint type"].append(cstrnt_types[i])

python/arm_ik_wbc/inverse_kinematics_task.py
@@ -49,6 +49,7 @@ class InverseKinematicsTask:
 
     def IKb(self, target_pos, target_ori, dt):
         Nonetype = type(None)
+        target_ori_local = np.copy(target_ori)
         # Find the position of the frame through forward kinematics
         fk_pos = np.copy(self.robot_data.oMf[self.frame_index].translation).reshape(3,1)
         #fk_ori = R.from_matrix(np.copy(self.robot_data.oMf[self.frame_index].rotation))
@@ -57,23 +58,30 @@ class InverseKinematicsTask:
         pos_vel = ref_vel + (np.dot(self.b_pos_gain, ((target_pos - fk_pos)/dt)))
 
         # Find the angular velocity
+        # Check for Euler wrapping
+        #for i in range(len(target_ori)):
+        #    if target_ori_local[i] >= math.pi/2:
+        #        target_ori_local[i] = target_ori_local[i] - (math.pi/2)
+        
         fk_ori = R.from_matrix(np.copy(self.robot_data.oMf[self.frame_index].rotation)) # rot mat at t
         
-        #fk_ori = fk_ori.as_quat() # q at t
-        #target_ori_q = R.from_euler('xyz', target_ori.reshape(3,)) # euler at t+1
-        #target_ori_q = target_ori_q.as_quat().reshape(4,) # q at t+1
-        #prev_ori = R.from_euler('xyz', self.prev_ori.reshape(3,))
-        #prev_ori_q = prev_ori.as_quat()
-        #w = self.ang_vel_from_quat(fk_ori, target_ori_q, dt)
-        #w_vel = self.ang_vel_from_quat(prev_ori_q, target_ori_q, dt)
-        #ori_vel = (w_vel + w).reshape(3,1)
-        #ori_vel = np.dot(ori_vel, self.b_ori_gain)
-        
-        fk_ori_euler = fk_ori.as_euler('xyz')
-        ori_error = ((target_ori.reshape(3,) - fk_ori_euler)/dt).reshape(3,)
-        w = np.dot(ori_error, self.b_ori_gain)
-        w_vel = (target_ori.reshape(3,) - self.prev_ori.reshape(3,))/dt
+        fk_ori = fk_ori.as_quat() # q at t
+        target_ori_q = R.from_euler('xyz', target_ori.reshape(3,)) # euler at t+1
+        target_ori_q = target_ori_q.as_quat().reshape(4,) # q at t+1
+        prev_ori = R.from_euler('xyz', self.prev_ori.reshape(3,))
+        prev_ori_q = prev_ori.as_quat()
+        w = self.ang_vel_from_quat(fk_ori, target_ori_q, dt)
+        w = np.dot(w, self.b_ori_gain)
+        w_vel = self.ang_vel_from_quat(prev_ori_q, target_ori_q, dt)
         ori_vel = (w_vel + w).reshape(3,1)
+
+        
+        #fk_ori_euler = fk_ori.as_euler('xyz')
+        #ori_error = ((target_ori.reshape(3,) - fk_ori_euler)/dt).reshape(3,)
+        #w = np.dot(ori_error, self.b_ori_gain)
+        #w_vel = (target_ori.reshape(3,) - self.prev_ori.reshape(3,))/dt
+        #w_vel = np.dot(w_vel, self.b_ori_gain)
+        #ori_vel = (w_vel+w).reshape(3,1)
         
         
 